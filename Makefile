cff8882607a11fd06f87f36a84cdf4ae9104cc22
Hans Löfving <hans.lofving@ericsson.com>
2019-12-05 09:57:57 +0100

    day 5
    Change-Id: I8d8936426ff4765be29402433a48704bfe9499ed

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all: $(patsubst %.c,bin/%,$(wildcard *.c))
 
+bin/prog2 bin/prog5: intcode.h
+
 bin/%: %.c bin/.STAMP
 	gcc -g -O3 -std=c99 $< -o $@
 
---
+++ prog5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "intcode.h"
+
+int intcode_prog[] = {
+#include "input5"
+};
+
+int main(int argc, char * argv[])
+{
+	int ret;
+	ret = run_prog_with_input(intcode_prog, PROG_SIZE(intcode_prog), 1);
+	printf("Part 1: %d\n", ret);
+
+	ret = run_prog_with_input(intcode_prog, PROG_SIZE(intcode_prog), 5);
+	printf("Part 2: %d\n", ret);
+	// Part 1: 4601506
+	// Part 2: 5525561
+	return 0;
+}
