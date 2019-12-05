01d516513c1512e4e6b1daee1b0edbd100c3caf0
Hans Löfving <hans.lofving@ericsson.com>
2019-12-05 14:22:56 +0100

    day 3 cleanups
    Change-Id: I4189acf99f749d43765200b16d005eea6dbe85bc

--- prog3.c
+++ prog3.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct line {
+#define END_MARKER 0xFFFFFF
+
+struct segment {
 	int x; int y;
 	int len;
 	int index; // for debugging
@@ -10,10 +12,7 @@ struct line {
 	int ydelta;
 };
 
-struct line line1[1024];
-struct line line2[1024];
-
-void read_line(struct line * line)
+void read_line(struct segment * seg)
 {
 	char dir, slask;
 	int len;
@@ -22,28 +21,28 @@ void read_line(struct line * line)
 	int totsteps = 0;
 
 	while (scanf("%c%d%[,]", &dir, &len, &slask) >= 2) {
-		line[j].x = x;
-		line[j].y = y;
-		line[j].len = len;
-		line[j].index = j;
-		line[j].totsteps = totsteps;
+		seg[j].x = x;
+		seg[j].y = y;
+		seg[j].len = len;
+		seg[j].index = j;
+		seg[j].totsteps = totsteps;
 		switch (dir) {
-			case 'L': x = x - len; line[j].xdelta = -len; break;
-			case 'R': x = x + len; line[j].xdelta =  len; break;
-			case 'U': y = y - len; line[j].ydelta = -len; break;
-			case 'D': y = y + len; line[j].ydelta =  len; break;
+			case 'L': x = x - len; seg[j].xdelta = -len; break;
+			case 'R': x = x + len; seg[j].xdelta =  len; break;
+			case 'U': y = y - len; seg[j].ydelta = -len; break;
+			case 'D': y = y + len; seg[j].ydelta =  len; break;
 		}
 		totsteps += len;
 		j++;
 	}
-	line[j].x = 0xFFFFFF;
-	line[j].y = 0xFFFFFF;
+	seg[j].x = END_MARKER;
+	seg[j].y = END_MARKER;
 }
 
-struct line closest_man = { .len = 0xFFFFFF };
-struct line closest_tot = { .len = 0xFFFFFF };
+struct segment closest_man = { .len = END_MARKER };
+struct segment closest_tot = { .len = END_MARKER };
 
-void match(struct line l1, struct line l2)
+void check_closest(struct segment l1, struct segment l2)
 {
 	int x, y;
 	int totsteps = l1.totsteps + l2.totsteps;
@@ -67,12 +66,9 @@ void match(struct line l1, struct line l2)
 		//printf("man! %d,%d/%d\t%d,%d/%d\t@%d,%d [%d][%d]\n", l1.x, l1.y, l1.len, l2.x, l2.y, l2.len, x, y, l1.index, l2.index);
 	}
 	if (totsteps < closest_tot.len) {
-		closest_tot.x = x;
-		closest_tot.y = y;
 		closest_tot.len = totsteps;
 		//printf("tot! %d,%d/%d\t%d,%d/%d\t@%d,%d [%d][%d]\n", l1.x, l1.y, l1.len, l2.x, l2.y, l2.len, x, y, l1.index, l2.index);
 	}
-
 }
 
 int within(int p1, int p2, int p)
@@ -85,21 +81,23 @@ int within(int p1, int p2, int p)
 		min = p2;
 		max = p1;
 	}
-	if (p < min) return 0;
+	if (p < min) {
+		return 0;
+	}
 	return (p <= max);
 }
 
-void intersect(struct line l1, struct line l2)
+void check_intersection(struct segment l1, struct segment l2)
 {
 	if (l1.ydelta == 0) {
 		if (l2.ydelta == 0) return;
 		if (within(l1.x, l1.x + l1.xdelta, l2.x) && within(l2.y, l2.y + l2.ydelta, l1.y)) {
-			match(l1, l2);
+			check_closest(l1, l2);
 		}
 	} else { // if l1.ydelta != 0 then l1.xdelta must be 0, if input can be trusted to not have 0 length
 		if (l2.xdelta == 0) return;
 		if (within(l2.x, l2.x + l2.xdelta, l1.x) && within(l1.y, l1.y + l1.ydelta, l2.y)) {
-			match(l1, l2);
+			check_closest(l1, l2);
 		}
 	}
 }
@@ -108,19 +106,17 @@ int main(int argc, char * argv[])
 {
 	int w;
 	int tot = 0;
+	struct segment line1[1024];
+	struct segment line2[1024];
+
 	read_line(line1);
 	scanf("\n");
 	read_line(line2);
 
-	int i = 0, j = 0;
-
-	while (line1[i + 1].x != 0xFFFFFF) {
-		j = 0;
-		while (line2[j + 1].x != 0xFFFFFF) {
-			intersect(line1[i], line2[j]);
-			j++;
+	for (int i = 0; line1[i].x != END_MARKER; i++) {
+		for (int j = 0; line2[j].x != END_MARKER; j++) {
+			check_intersection(line1[i], line2[j]);
 		}
-		i++;
 	}
 
 	printf("Closest manhattan discance intersection: %d\n", abs(closest_man.x) + abs(closest_man.y));
