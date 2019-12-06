a7bf9eb3614beae6c7a2254e807ba550ff168e97
Hans Löfving <hans.lofving@ericsson.com>
2019-12-06 22:27:57 +0100

    merge day1 part 1 and 2 into the same file
    Change-Id: I1d3e0151ca676057a4b8ab71b90603bc03fedc2c

--- prog1.c
+++ prog1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-
 int main(int argc, char * argv[])
 {
 	int w;
-	int tot = 0;
+	int tot1 = 0;
+	int tot2 = 0;
 	while (scanf("%d\n", &w) == 1) {
 		int tmp = (w / 3) - 2;
+		tot1 += tmp;
 		while (tmp > 0) {
-			tot += tmp;
+			tot2 += tmp;
 			tmp = (tmp / 3) - 2;
 		}
 	}
-	printf("%d\n", tot);
+	printf("Part1: %d\n", tot1);
+	printf("Part2: %d\n", tot2);
 }
 
