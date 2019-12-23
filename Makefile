6f123041e536ad79ce0065b0f57bd3ee94524b6c
Hans Löfving <hans.lofving@ericsson.com>
2019-12-23 08:12:37 +0100

    day 23 part 2
    Change-Id: I531f522521f5b6455fb11c11645021e72e42957a

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ all: $(patsubst %.c,bin/%,$(wildcard *.c))
 
 CFLAGS += -Wall -g -O3 -D_BSD_SOURCE -std=c99 -lm
 
-bin/prog2 bin/prog5 bin/prog7 bin/prog9 bin/prog11: intcode.h
+bin/prog2  bin/prog5  bin/prog7  bin/prog9  bin/prog11 bin/prog13: intcode.h
+bin/prog15 bin/prog17 bin/prog19 bin/prog21 bin/prog23: intcode.h
 
 bin/%: %.c bin/.STAMP Makefile
 	gcc $(CFLAGS) $< -o $@
--- prog23.c
+++ prog23.c
@@ -14,6 +14,12 @@ int64_t program[] = {
 
 struct intcode_machine * nodes[50];
 
+int64_t nat_x = 0;
+int64_t nat_y = 0;
+
+int64_t part1 = 0;
+int64_t part2 = 0;
+
 int main(int argc, char * argv[])
 {
 	for (int i = 0; i < 50; i++) {
@@ -21,26 +27,46 @@ int main(int argc, char * argv[])
 	}
 
 	int count = 0;
+	int idle = 0;
+	int last_nat = 0;
 	while (1) {
+		idle++;
 		for (int i = 0; i < 50; i++) {
 			if (EMPTY(nodes[i]->inputs)) {
+				nodes[i]->inputs.read = 0;
+				nodes[i]->inputs.write = 0;
 				PUT(nodes[i]->inputs, -1);
+			} else {
+				idle = 0;
 			}
 			run_machine(nodes[i]);
 			if (!EMPTY(nodes[i]->outputs)) {
 				int64_t addr = GET(nodes[i]->outputs);
 				int64_t x = GET(nodes[i]->outputs);
 				int64_t y = GET(nodes[i]->outputs);
-				printf("%d: %d -> %ld {%ld,%ld)\n", count, i, addr, x, y);
 				if (addr == 255) {
-					printf("DONE! %ld\n", y);
-					return (0);
+					if (part1 == 0) part1 = y;
+					// empty NAT queue, (always overwrite)
+					nat_x = x;
+					nat_y = y;
+				} else {
+					PUT(nodes[addr]->inputs, x);
+					PUT(nodes[addr]->inputs, y);
 				}
-				PUT(nodes[addr]->inputs, x);
-				PUT(nodes[addr]->inputs, y);
 			}
+		}
+		if (idle > 1) {
+			if (last_nat == nat_y) {
+				part2 = nat_y;
+				break;
+			}
+			last_nat = nat_y;
+			PUT(nodes[0]->inputs, nat_x);
+			PUT(nodes[0]->inputs, nat_y);
 		}
 		count++;
 	}
+	printf("Part1: %ld\n", part1);
+	printf("Part2: %ld\n", part2);
 }
 
