a6e690185e078d51a21e94158c025e53d489a88d
Hans Löfving <hans.lofving@ericsson.com>
2019-12-23 08:13:17 +0100

    day 16 part 2 - runtime ~800 seconds. probably not the best solution
    Change-Id: Id44b2788ec8a923484331a4982fcc5a832bcefb2

--- prog16.c
+++ prog16.c
@@ -37,14 +37,147 @@ void run_phase(int * input, int * output, int len)
 	}
 }
 
-int * part1(int * input, int len)
+struct moving_sum {
+	char * start;
+	char * end;
+	int sum;
+};
+
+int update_sum_start(struct moving_sum * m, int i, char * end)
+{
+	char * new_start = m->start + i + i + 1;
+	int retval = 0;
+	if (new_start >= end) {
+		new_start = end - 1;
+		retval = 1;
+		m->sum = 0;
+	}
+	while (m->start < new_start) {
+		m->sum -= m->start[0];
+		m->start++;
+	}
+	return retval;
+}
+
+int update_sum_end(struct moving_sum * m, int i, char * end)
+{
+	char * new_end = m->end + i + i + 2;
+	int retval = 0;
+	if (new_end >= end) {
+		new_end = end - 1;
+		retval = 1;
+	}
+	while (m->end < new_end) {
+		m->end++;
+		m->sum += m->end[0];
+	}
+	return retval;
+}
+
+#define NSUMS 1000
+struct moving_sum slask = { 0, 0, 0 };
+struct moving_sum moving_sums[NSUMS] = { [0 ... NSUMS-1] = { 0, 0, 0 } };
+struct moving_sum slbsk = { 0, 0, 0 };
+
+void run_phase_x(char * input, char * output, int len)
+{
+
+	// NOTE using chars instead of ints reduces time by almost 50%. (probably because less memory to fetch into cache)
+	// NOTE doing more additions, subtractions inside the loop barey increases the run time
+
+	int nsums = len / 10000 + 1;
+	int64_t sum = 0;
+
+	for (int i = 0; i < nsums; i++) {
+		moving_sums[i].start = &input[-1];
+		moving_sums[i].end = &input[-2];
+		moving_sums[i].sum = 0;
+	}
+
+	int last_start_end_sum = nsums - 1;
+	int last_start_sum = nsums - 1;
+
+	for (int i = 0; i < len; i++) { // for each output value
+		sum = 0;
+		for (int j = 0; j <= last_start_end_sum; j++) {
+			if (update_sum_end(&moving_sums[j], j, &input[len])) {
+				last_start_end_sum -= 1;
+				//printf("SE-- %d %d %d\n", i, last_start_sum, last_start_end_sum);
+			}
+		}
+		for (int j = 0; j <= last_start_sum; j++) {
+			if (update_sum_start(&moving_sums[j], j, &input[len])) {
+				last_start_sum -= 1;
+				//printf("S-- %d %d %d\n", i, last_start_sum, last_start_end_sum);
+				break;
+			}
+			if (j % 2 == 0) {
+				sum += moving_sums[j].sum;
+			} else {
+				sum -= moving_sums[j].sum;
+			}
+
+		}
+		/*
+		if (i < 3) { 
+			printf("%d update: %ld %ld %d\n", i, moving_sums[0].start - &input[0], moving_sums[0].end - &input[0], (int)moving_sums[0].sum);
+			printf("           %ld %ld %d\n", moving_sums[1].start - &input[0], moving_sums[1].end - &input[0], (int)moving_sums[1].sum);
+			printf("           %ld %ld %d\n", moving_sums[2].start - &input[0], moving_sums[2].end - &input[0], (int)moving_sums[2].sum);
+		}
+		*/
+		if (last_start_end_sum < nsums - 1) {
+			output[i] = abs(sum) % 10;
+			continue;
+		}
+
+		int incr = i + 1;
+		//printf("%d %d %ld\n", i, last_start_end_sum, moving_sums[last_start_end_sum].end - &input[0]);
+		for (char * p = moving_sums[last_start_end_sum].end + 1 + incr; &p[0] < &input[len]; ) {
+			char * end = &p[incr];
+			int tempsum = 0;
+			if (end > &input[len]) end = &input[len];
+			for (; p < end; p++) {
+				tempsum += *p;
+			}
+			if (last_start_end_sum % 2 == 0) {
+				//printf("%d minus\n", i);
+				sum -= tempsum;
+			} else {
+				//printf("%d plus\n", i);
+				sum += tempsum;
+			}
+
+			p += incr;
+			if (&p[0] >= &input[len]) break;
+
+			end = &p[incr];
+			tempsum = 0;
+			if (end > &input[len]) end = &input[len];
+			for (; p < end; p++) {
+				tempsum += *p;
+			}
+			if (last_start_end_sum % 2 == 0) {
+				sum += tempsum;
+			} else {
+				sum -= tempsum;
+			}
+			p += incr;
+		}
+
+		output[i] = abs(sum) % 10;
+		//if (i % 1000 == 0) { write(STDOUT_FILENO, ".", 1); }
+	}
+	
+}
+
+int * part1(int * input, int phases, int len)
 {
 	int * temp1 = calloc(sizeof(int), len);
 	int * temp2 = calloc(sizeof(int), len);
 
 	int * inp = input;
 	int * outp = temp1;
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < phases; i++) {
 		run_phase(inp, outp, len);
 		if (outp == temp1) {
 			inp = temp1;
@@ -55,41 +188,36 @@ int * part1(int * input, int len)
 		}
 	}
 
+	// 29956495
+	printf("Part1: ");
+	for (int i = 0; i < 8; i++)
+		printf("%d", (int)inp[i]);
+	printf("\n");
+
 	return inp;
 }
 
-void run_phase_x(int * input, int * output, int len)
+char * part2(int * input, int phases, int input_len, int multiplier)
 {
-	int64_t sum = 0;
-	int64_t sum2 = 0;
-	int * end = &input[len - 2];
-	//printf("%ld %d\n", end - &input[0], len);
-	// XXX opportunity for cache hit/miss experiments. one loop instead of two!
-	int * p;
-	for (p = &input[0]; p < end; p += 4) {
-		sum += p[0];
-		sum -= p[2];
-		//sum2 += p[0];
+	int len = input_len * multiplier;
+	char * temp1 = calloc(1, len);
+	char * temp2 = calloc(1, len);
+
+	for (int i = 0; i < len; i++) {
+		temp1[i] = (char)input[i % input_len];
 	}
-	if (&p[2] < &input[len]) sum += p[2];
 
-	output[1] = sum2;
-	output[0] = abs(sum) % 10;
-}
+	char * inp = temp1;
+	char * outp = temp2;
+	int offset = 0;
+	int slask = 1;
+	for (int k = 6; k >= 0; k--) {
+		offset += inp[k] * slask;
+		slask = slask * 10;
+	}
 
-int * part2(int * input, int input_len)
-{
-	int len = input_len * 10000;
-	int * temp1 = calloc(sizeof(int), len);
-	int * temp2 = calloc(sizeof(int), len);
-
-	printf("X\n");
-	for (int i = 0; i < len; i++) temp1[i] = input[i % input_len];
-	printf("X\n");
-
-	int * inp = temp1;
-	int * outp = temp2;
-	for (int i = 0; i < 1000; i++) {
+	printf("first seven? %d\n", offset);
+	for (int i = 0; i < phases; i++) {
 		run_phase_x(inp, outp, len);
 		if (outp == temp1) {
 			inp = temp1;
@@ -98,8 +226,20 @@ int * part2(int * input, int input_len)
 			inp = temp2;
 			outp = temp1;
 		}
+		printf(">>> %d\n", i);
 	}
 
+	// read 7 at start. skip that many!
+	//printf("Part2: ");
+	//for (int i = 0; i < 8; i++)
+		//printf("%d", inp[i]);
+	//printf("\n");
+
+	
+	printf("Part2: ");
+	for (int i = offset; i < 8 + offset; i++)
+		printf("%d", inp[i]);
+	printf("\n");
 	return inp;
 }
 
@@ -113,33 +253,40 @@ int main(int argc, char * argv[])
 	int * values = calloc(sizeof(int), len);
 	for (int i = 0; i < len; i++) values[i] = input[i] - '0';
 
-	//int * part1_out = part1(values, len);
+	int phases = 100;
+	int * part1_out = part1(values, phases, len);
+	(void) part1_out;
 
-	//for (int i = 0; i < 8; i++)
-		//printf("%d", part1_out[i]);
-	//printf("\n");
-
+	char * part2_out = part2(values, phases, len, 10000);
+	(void) part2_out;
 	/*
-	int inputx[] = { 0, 1, 2, 3 };
-	int outputx[sizeof(inputx) / sizeof(int)];
-	run_phase(inputx, outputx, sizeof(inputx) / sizeof(int));
-	for (int i = 0; i < sizeof(inputx) / sizeof(int); i++)
-		printf("%d ", outputx[i]);
-	printf("\n");
+	int matching = 2;
+	for (int i = 0; i < len; i++) {
+		if (part1_out[i] == part2_out[i]) {
+			if (matching != 1) {
+				printf("matching from %d\n", i);
+			}
+			matching = 1;
+		} else {
+			if (matching != 0) {
+				printf("not matching from %d\n", i);
+			}
+			matching = 0;
+		}
+	}
 	*/
 
+
 	// output graphic representation of what to sum / subtract
+	/*
 	for (int i = 0; i < 50; i++) {
 		for (int j = 0; j < 230; j++) {
 			printf("%c", cpattern(i + 1, j));
 		}
 		printf("\n");
 	}
+	*/
 
-	int * part2_out = part2(values, len);
-	for (int i = 0; i < 8; i++)
-		printf("%d ", part2_out[i]);
-	printf("\n");
 
 	return 0;
 }
